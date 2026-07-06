89f9d7fdf499831e6147887d666e91232fc52292
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-07-07 01:22:16 +0530

    Update basics.java

--- Array/basics.java
+++ Array/basics.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class basics {
+    public static void main(String[] args) {
+        int marks [] = {97, 98, 100, 95, 96};
+        String students [] = new String [5];
+        // data type array name [] = new data type [size];
+        Scanner sc  =  new Scanner(System.in);
+        for(int i = 0; i < students.length; i++){
+            students[i] = sc.nextLine();
+        }
+
+        for(int i = 0; i < students.length; i++){
+            System.out.print(students[i] + " ");
+        }
+    }
+}
