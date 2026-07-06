e74bc404b159b7b1b0403785b69c920a49015fdf
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-07-07 01:22:34 +0530

    Update LinearSearch.java

--- Array/LinearSearch.java
+++ Array/LinearSearch.java
@@ -1,1 +1,16 @@
-
+public class LinearSearch {
+    public static int Search(String menu[], String key){
+        for(int i = 0; i < menu.length; i++){
+            if(key == menu[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        String menu[] = {"Vada Pav", "Samosa", "Chaat", "Chole Bhature", "Coffee", "Latte"};
+        String key = "Coffee";
+        int result = Search(menu, key);
+        System.out.print(result);
+    }
+}
